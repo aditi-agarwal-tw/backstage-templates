7962f32108851791ce5258fa4d8f72dcd790c788
Aditi Agarwal <aditi.agarwal@thoughtworks.com>
2022-09-10 07:33:15 +0000

    commit

---
+++ springboot-kotlin/content/src/test/kotlin/${{values.javaDir}}/ApplicationTest.kt
@@ -0,0 +1,14 @@
+package ${{values.javapackage}}
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    fun applicationLoads() {
+
+    }
+}
